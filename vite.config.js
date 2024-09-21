51dc122a10f98ef43d36e8d7980fc90bcd5d51dc
mursidx <iammursid@gmail.com>
2024-09-21 11:09:34 +0530

    title changed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-webX', // Replace 'my-app' with your repository name
+  base: '/react-webx/',
 });
